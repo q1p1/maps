a87a94d8c6b43691c49d5bb46f7367865d9344e7
q1p1 <meshari.algahbban@wakecap.com>
2024-10-13 16:51:06 +0300

    main

--- src/App.tsx
+++ src/App.tsx
@@ -5,46 +5,17 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
-// import SignIn from "./components/SignIn";
-// import Sup from "./components/Sup";
 import MapComponent from "./components/MapComponent";
+
 const App: React.FC = () => {
-  const isLoggedIn = () => {
-    const username = localStorage.getItem("username");
-    const password = localStorage.getItem("password");
-    return username && password;
-  };
-
-  React.useEffect(() => {
-    localStorage.clear();
-  }, []);
-
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            isLoggedIn() ? <Navigate to="/map" /> : <Navigate to="/signin" />
-          }
-        />
+        <Route path="/" element={<MapComponent />} />
 
-        {/* <Route
-          path="/signin"
-          element={isLoggedIn() ? <Navigate to="/map" /> : <SignIn />}
-        />
+        <Route path="/map" element={<MapComponent />} />
 
-        <Route
-          path="/signup"
-          element={isLoggedIn() ? <Navigate to="/map" /> : <Sup />}
-        /> */}
-
-        <Route
-          path="/map"
-          element={isLoggedIn() ? <MapComponent /> : <Navigate to="/signin" />}
-        />
-
-        <Route path="*" element={<Navigate to="/signin" />} />
+        <Route path="*" element={<Navigate to="/map" />} />
       </Routes>
     </Router>
   );
